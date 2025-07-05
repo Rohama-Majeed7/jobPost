30ebbbae948adca7959a6a8d699eececf6e7638d
Rohama-Majeed7 <rohamamajeed3@gmail.com>
2025-07-05 14:23:47 +0500

    change

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
+// Use a global variable to persist the Prisma client in development
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient;
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ??
+  new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Avoid multiple instances of Prisma Client in dev (hot reload safe)
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export { prisma };
